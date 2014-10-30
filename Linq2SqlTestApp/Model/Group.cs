7acbca01bb8583928683f64aa2e93a434951c8c5
KarolV <karol.vecerek@gmail.com>
2014-10-30 22:42:25 +0100

    Initial version of the test application

---
+++ Linq2SqlTestApp/Model/Group.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace Linq2SqlTestApp.Model
+{
+	[Table(Name = "Group")]
+	public sealed class Group
+	{
+		private Guid _groupID;
+		private readonly EntitySet<Affiliate> _affiliates;
+		private string _name;
+
+		public Group()
+		{
+			_affiliates = new EntitySet<Affiliate>();
+		}
+
+		[Column(IsPrimaryKey = true, Storage = "_groupID", IsDbGenerated = true)]
+		public Guid GroupID
+		{
+			get { return _groupID; }
+			set { _groupID = value; }
+		}
+
+		[Column(Storage = "_name", DbType = "nvarchar(50)")]
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+
+		[Association(Storage = "_affiliates", OtherKey = "AffiliateID")]
+		public EntitySet<Affiliate> Affiliates
+		{
+			get { return _affiliates; }
+			set { _affiliates.Assign(value); }
+		}
+	}
+}
